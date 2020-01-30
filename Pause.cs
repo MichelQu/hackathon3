73780155b43695f7f4508d3f1915b8b4217cc40b
MwanPygmay <59645813+MwanPygmay@users.noreply.github.com>
2020-01-30 15:05:58 +0100

    ajout menu pause
     il faut ajouter les bonnes commandes pour le controller

---
+++ Pause.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Pause : MonoBehaviour
+{
+    private bool isPaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            isPaused = !isPaused;
+        if (isPaused)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1f;
+
+    }
+
+    void OnGUI()
+    {
+        if (isPaused)
+        {
+            if(GUI.Button(new Rect(Screen.width/2 - 40, Screen.height/2 -20, 80, 40), "Continuer"))
+            {
+                isPaused = false;
+            }
+        }
+    }
+}
