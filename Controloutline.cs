7910776647eb38fae17e58b4ae7a9a2fb5135fc2
MwanPygmay <59645813+MwanPygmay@users.noreply.github.com>
2020-01-30 18:15:51 +0100

    shader outline+scipt controller
    shader outline à mettre sur les planetes et le script qui va avec qui controle:
    -révisions à faire --> changer onmouse par  raycast

---
+++ Controloutline.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controloutline : MonoBehaviour
+{
+
+    void OnMouseOver()
+    {
+        GetComponent<Renderer>().material.SetFloat("_FirstOutlineWidth", 0.02f); 
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.SetFloat("_FirstOutlineWidth", 0f);
+    }
+}
